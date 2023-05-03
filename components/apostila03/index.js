1a0adbc3625088cdfa2a061e31925fe707768a40
carvalhxx <kayky.carvalho2@etec.sp.gov.br>
2023-05-02 21:23:47 -0300

    02/05

---
+++ components/apostila03/index.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Index() {
+    return(
+        <View style={StyleSheet.container} >
+            <Text style={StyleSheet.paragraph} >
+                Exemplo 3
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#F5F5F5',
+        padding: 8,
+    },
+    paragraph : {
+        margin: 24,
+        fontSize: 26,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#   9E9E9E',
+    },
+});
